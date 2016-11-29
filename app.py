3eabc1abaf9628ed702c49b8ddf9273723cb5523
Katherine Pully <katpully@usnycrad6701.global.publicisgroupe.net>
2016-11-29 09:46:24 -0500

    ajax

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
 import flask
+import pandas as pd
+
+app = flask.Flask(__name__)
+
+names = pd.read_csv('justices.csv')
+
+@app.route("/justices", methods = ["POST"])
+def justices():
+	data = flask.request.json
+	results = {'test': 'test'}
+	return flask.jsonify(results)
+
+
+
+
+
+
+
+app.run(debug=True)
