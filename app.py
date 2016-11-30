22128c1a860d813a2545248800a387fd016db333
Katherine Pully <katpully@usnycrad6701.global.publicisgroupe.net>
2016-11-30 11:38:33 -0500

    updates to flask app

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
-import flask
+from flask import Flask, render_template, request, jsonify
 import pandas as pd
+import os
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 names = pd.read_csv('justices.csv')
 
-@app.route("/justices", methods = ["POST"])
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/justices')
 def justices():
-	data = flask.request.json
-	results = {'test': 'test'}
-	return flask.jsonify(results)
+	year = request.args.get('year', 0, type=int)
+	return jsonify(result=year)
 
 
-
-
-
-
-
-app.run(debug=True)
+if __name__ == '__main__':
+    app.debug= True
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
